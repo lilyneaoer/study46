7bf58fe3e37e52095a9435abe3c08bded8b8f829
maxuehao <lilyneao@foxmail.com>
2026-07-06 17:10:09 +0800

    修复总数统计逻辑

--- src/main/java/com/snow/study46/repository/UserRepository.java
+++ src/main/java/com/snow/study46/repository/UserRepository.java
@@ -10,6 +10,7 @@ package com.snow.study46.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -22,10 +23,10 @@ public interface UserRepository extends BaseMapper<User> {
   // and username like concat('%', #{userListDTO.username}, '%') and create_time >
   // #{userListDTO.startTime} and create_time < #{userListDTO.endTime} limit
   // #{userListDTO.pageSize} offset #{offset}")
-  public List<User> getUserList(UserListDTO userListDTO, int offset);
+  public List<User> getUserList(@Param("userListDTO") UserListDTO userListDTO, @Param("offset") int offset);
 
   // @Select("select count(*) from user")
-  public int getCountUser();
+  public int getCountUser(@Param("userListDTO") UserListDTO userListDTO);
   // myBatis
   // // 查询
   // @Select("select * from user where username = #{username}")
